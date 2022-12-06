82c80a225c55805b42738bf88539b2f0a097a145
Andrew <drewcronin00@gmail.com>
2022-12-05 22:21:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,45 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
 """
-# Welcome to Streamlit!
+# Andrew Cronin's Final Project!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+This should predict 
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    
+df = pd.read_csv('highest.csv', header =0)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+y_data = df['WealthDegree']
+y_data = y_data.astype('int')
 
-    points_per_turn = total_points / num_turns
+x_data = df.drop('WealthDegree', axis = 1)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.3)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+clf = DecisionTreeClassifier(random_state=123)
+params =  {
+    'min_weight_fraction_leaf': [0.1, 0.2, 0.3, 0.4, 0.5],
+    'max_depth': [1, 3, 5, 7, 9, 11],
+    'max_leaf_nodes':[None,20,40,60,80] 
+}
+grid = GridSearchCV(estimator=clf,
+                    param_grid=params,
+                    cv=10,
+                    n_jobs=1,
+
+)
+grid.fit(x_train, y_train)
+print(grid.best_score_)
+print(grid.best_params_)
